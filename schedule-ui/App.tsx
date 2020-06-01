d6b83f063d35af8d0f8840deb571f2ef1a67189b
Justin Tan <justin@192-168-1-3.tpgi.com.au>
2020-06-01 20:34:41 +1000

    Started layout using flexbox

--- schedule-ui/App.tsx
+++ schedule-ui/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
+import Tasks from "./tasks";
 
 const App = () => {
   return (
-    <div>
+    <div className="container">
       <div className="sch-header">header</div>
-      <div className="sch-body">body</div>
+      <div className="sch-body">
+        <Tasks />
+      </div>
       <div className="sch-footer">footer</div>
     </div>
   );
---
+++ schedule-ui/Tasks.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Tasks = () => {
+  return (
+    <div className="main-content">
+      <button className="add">Add</button>
+      <button className="save">Save</button>
+      <div>okay</div>
+    </div>
+  );
+};
+
+export default Tasks;
